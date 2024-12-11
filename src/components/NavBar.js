ff2f11cdfce7f2ddf88e59849b0efc697775c086
Petra <petra@behemot.biz>
2024-12-11 13:32:40 +0100

    feat: Add NavBar, routes, SignUp and SignIn pages

---
+++ src/components/NavBar.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { Navbar, Container, Nav } from 'react-bootstrap';
+import styles from '../styles/NavBar.module.css';
+import { NavLink } from 'react-router-dom/cjs/react-router-dom';
+
+const NavBar = () => {
+  return (
+    <Navbar className={styles.NavBar} expand="md" fixed='top'>
+    <Container>
+        <NavLink to='/'>
+            <Navbar.Brand>TastyTales</Navbar.Brand>
+        </NavLink>
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+        <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="ml-auto text-left">
+                <NavLink exact 
+                className={styles.NavLink} 
+                activeClassName={styles.Active} 
+                to='/'>Home</NavLink>
+                <NavLink className={styles.NavLink} activeClassName={styles.Active}  to='/signin'>Sign in</NavLink>
+                <NavLink className={styles.NavLink} activeClassName={styles.Active}  to='/signup'>Sign up</NavLink>
+
+            </Nav>
+        
+        </Navbar.Collapse>
+    </Container>
+    </Navbar>
+    )
+}
+
+export default NavBar
