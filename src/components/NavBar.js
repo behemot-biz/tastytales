18292cab2a81696860cd6a092a215d1b10bfa5b5
Petra <petra@behemot.biz>
2024-12-11 13:59:40 +0100

    feat: Setup useContext hook

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,31 +1,53 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Navbar, Container, Nav } from 'react-bootstrap';
 import styles from '../styles/NavBar.module.css';
 import { NavLink } from 'react-router-dom/cjs/react-router-dom';
+import { CurrentUserContext } from '../App';
 
 const NavBar = () => {
+    const currentUser = useContext(CurrentUserContext)
+    const loggedInIcons = <>{currentUser?.username}</>;
+    const loggedOutIcons = (
+      <>
+        <NavLink
+          className={styles.NavLink}
+          activeClassName={styles.Active}
+          to="/signin"
+        >
+          Sign in
+        </NavLink>
+        <NavLink
+          className={styles.NavLink}
+          activeClassName={styles.Active}
+          to="/signup"
+        >
+          Sign up
+        </NavLink>
+      </>
+    );
   return (
-    <Navbar className={styles.NavBar} expand="md" fixed='top'>
-    <Container>
-        <NavLink to='/'>
-            <Navbar.Brand>TastyTales</Navbar.Brand>
+    <Navbar className={styles.NavBar} expand="md" fixed="top">
+      <Container>
+        <NavLink to="/">
+          <Navbar.Brand>TastyTales</Navbar.Brand>
         </NavLink>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ml-auto text-left">
-                <NavLink exact 
-                className={styles.NavLink} 
-                activeClassName={styles.Active} 
-                to='/'>Home</NavLink>
-                <NavLink className={styles.NavLink} activeClassName={styles.Active}  to='/signin'>Sign in</NavLink>
-                <NavLink className={styles.NavLink} activeClassName={styles.Active}  to='/signup'>Sign up</NavLink>
-
-            </Nav>
-        
+          <Nav className="ml-auto text-left">
+            <NavLink
+              exact
+              className={styles.NavLink}
+              activeClassName={styles.Active}
+              to="/"
+            >
+              Home
+            </NavLink>
+            {currentUser ? loggedInIcons : loggedOutIcons}
+          </Nav>
         </Navbar.Collapse>
-    </Container>
+      </Container>
     </Navbar>
-    )
+  );
 }
 
 export default NavBar
