88c57ad550e7755c58eaa2512c11ad2657e06d3a
Petra <petra@behemot.biz>
2024-12-14 17:09:55 +0100

    Feat: Improve mobile NavBar  toggle with custom hook

---
+++ src/hooks/useClickOutsideToggle.js
@@ -0,0 +1,25 @@
+import React, { useEffect, useRef, useState } from "react";
+
+const useClickOutsideToggle = () => {
+  const [expanded, setExpanded] = useState(false);
+  const ref = useRef(null);
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (ref.current && !ref.current.contains(event.target)) {
+        setExpanded(false);
+      }
+    };
+    document.addEventListener("mouseup", handleClickOutside);
+    return () => {
+      document.removeEventListener("mouseup", handleClickOutside);
+    };
+  }, [ref]);
+
+  console.log("expanded: ", expanded);
+  console.log("setExpanded: ", setExpanded);
+  console.log("ref: ", ref);
+
+  return { expanded, setExpanded, ref };
+};
+
+export default useClickOutsideToggle;
